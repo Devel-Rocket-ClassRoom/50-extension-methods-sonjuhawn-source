8d8c6b10d4378bb80518ff567be19c28788a07b3
Son-JuHwan <sonjuhawn@gmail.com>
2026-03-13 17:00:28 +0900

    팔린드롬 완료

---
+++ Palindrome/Palindrome.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+static class Palindrome
+{
+    public static bool IsPalindrome(this string s)
+    {
+        if (string.IsNullOrEmpty(s)) return false;
+        string origin = s.Trim()
+                        .ToUpper()
+                        .Replace(",", "")
+                        .Replace(":", "")
+                        .Replace("?", "");
+        char[] strs = origin.ToCharArray();
+        char[] rev = strs.Reverse().ToArray();
+        for (int i = 0; i < rev.Length; i++)
+        {
+            if (!(rev[i] == strs[i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -1,4 +1,15 @@
 using System;
+using System.Linq;
 
 // README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+Console.WriteLine("=== 회문 판별 테스트 ===");
+Console.WriteLine($"\"토마토\" -> {"토마토".IsPalindrome()}");
+Console.WriteLine($"\"기러기\" -> {"기러기".IsPalindrome()}");
+Console.WriteLine($"\"level\" -> {"level".IsPalindrome()}");
+Console.WriteLine($"\"Level\" -> {"Level".IsPalindrome()}");
+Console.WriteLine($"\"A man, a plan, a canal: Panama\" -> {"A man, a plan, a canal: Panama".IsPalindrome()}");
+Console.WriteLine($"\"race a car\" -> {"race a car".IsPalindrome()}");
+Console.WriteLine($"\"hello\" -> {"hello".IsPalindrome()}");
+Console.WriteLine($"\"가나다\" -> {"가나다".IsPalindrome()}");
+Console.WriteLine($"\"\" -> {"".IsPalindrome()}");
+
